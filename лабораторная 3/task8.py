df7914e96bccbb70a41514f215d123d7ec4e5d20
Elena-Sheiko01 <115404364+Elena-Sheiko01@users.noreply.github.com>
2022-10-16 22:11:05 +0300

    Add files via upload

---
+++ лабораторная 3/task8.py
@@ -0,0 +1,14 @@
+money_capital = 10000
+salary = 5000
+spend = 6000
+increase = 0.05
+
+month = 0  # количество месяцев, которое можно прожить
+
+while money_capital > 0:
+    money_capital += (salary - spend)
+    month += 1
+    spend *= (1 + increase)
+    if money_capital + (salary - spend) <= 0:
+        break
+print(month)
